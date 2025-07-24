dc15dd991a55ebc2c3196ac94c25616a89979173
Kanika <avinash@Apples-Mac-mini.local>
2025-07-24 15:41:11 +0530

    db setup done

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require("cors");
-
+const connectDB = require('./config/db');
 const indexRouter = require('./routes/index');
 
 const app = express();
@@ -23,6 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+connectDB()
 
 // routes
 app.use('/', indexRouter);
