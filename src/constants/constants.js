d59a41fa56c95213f23ec9ecb786f7f29ed72696
Kanika <avinash@Apples-Mac-mini.local>
2025-07-24 17:06:29 +0530

    user model and const file setup

---
+++ src/constants/constants.js
@@ -0,0 +1,29 @@
+// HTTP Status Codes
+const HTTP_STATUS = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    CONFLICT: 409,
+    SERVER_ERROR: 500,
+};
+
+// Common Messages
+const MESSAGES = {
+    LOGIN_DONE: "Login done"
+};
+
+// Roles
+const ROLES = {
+    ADMIN: "admin",
+    USER: "user",
+};
+
+
+// Export all constants
+module.exports = {
+    HTTP_STATUS,
+    MESSAGES,
+    ROLES,
+};
---
+++ src/models/user.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const {ROLES} = require('../constants/constants.js');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, default: '' },
+    email: { type: String, default: ''},
+    password: { type: String, default: '' },
+    role: { type: String, enum: [ROLES.ADMIN, ROLES.USER], default: ROLES.USER },
+    otp: { type: String, default: '' },
+    isOtpVerified: { type: Boolean, default: false },
+    isActive: { type: Boolean, default: true },
+    deviceToken: { type: String, default: '' }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
